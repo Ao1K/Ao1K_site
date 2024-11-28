dbc157ecbcc18afa0cd7a3dbef8ab01a45aec0ff
Ao1K <averageofonethousand@gmail.com>
2024-11-27 20:26:49 -0600

    overspec nextconfig
    testing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  distDir: '.next'
+  output: 'standalone', // Ensures it builds correctly for deployment
+  trailingSlash: true, // Ensures Amplify correctly handles trailing slashes
+  distDir: '.next', // Specifies the directory for build artifacts
+  images: {
+    unoptimized: true, // Amplify doesn't optimize images by default
+  },
 };
 
 export default nextConfig;
