1c8bdf9a2636c1550e983f9cad665f79fb5cd197
Ao1K <averageofonethousand@gmail.com>
2024-11-27 22:07:02 -0600

    add next strict mode

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  reactStrictMode: true, // Optional but recommended
   output: 'standalone', // Ensures it builds correctly for deployment
   trailingSlash: true, // Ensures Amplify correctly handles trailing slashes
   distDir: '.next', // Specifies the directory for build artifacts
