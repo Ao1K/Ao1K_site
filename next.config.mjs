116373b198cddc3185fc670e05560f9a1ff11dea
Ao1K <averageofonethousand@gmail.com>
2024-11-27 23:05:13 -0600

    modify next.config

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,10 @@
 
 const nextConfig = {
   reactStrictMode: true, // Optional but recommended
-  output: 'standalone', // Ensures it builds correctly for deployment
+  // output: 'standalone', // Ensures it builds correctly for deployment
   trailingSlash: true, // Ensures Amplify correctly handles trailing slashes
   distDir: '.next', // Specifies the directory for build artifacts
+  assetPrefix: '.next', // not sure
   images: {
     unoptimized: true, // Amplify doesn't optimize images by default
   },
