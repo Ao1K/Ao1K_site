502d8d75306b3f9503103f440186671c96be0253
Ao1K <averageofonethousand@gmail.com>
2025-12-28 13:53:41 -0600

    Fix image generation 2
    Try changing encoding

--- app/recon/page.tsx
+++ app/recon/page.tsx
@@ -1,6 +1,7 @@
 import { Suspense } from 'react';
 import PageContent from '../../components/recon/_PageContent';
 import { Metadata, ResolvingMetadata } from 'next';
+import { customEncodeURL } from '../../composables/recon/urlEncoding';
 
 type Props = {
   params: Promise<{ id: string }>
@@ -14,10 +15,28 @@ export async function generateMetadata(
   const searchParams = await props.searchParams;
   const sp = new URLSearchParams();
   
-  if (searchParams.scramble) sp.set('scramble', searchParams.scramble as string);
-  if (searchParams.solution) sp.set('solution', searchParams.solution as string);
+  // apply custom encoding (spaces -> underscores) before setting params
+  // URLSearchParams.set() will then handle standard encoding (newlines, etc.)
+  // this matches the encoding used by updateURL
+  if (searchParams.scramble) {
+    let scramble = searchParams.scramble as string;
+    while (scramble.endsWith('\n')) scramble = scramble.slice(0, -1);
+    sp.set('scramble', customEncodeURL(scramble));
+  }
+  if (searchParams.solution) {
+    let solution = searchParams.solution as string;
+    while (solution.endsWith('\n')) solution = solution.slice(0, -1);
+    sp.set('solution', customEncodeURL(solution));
+  }
   if (searchParams.time) sp.set('time', searchParams.time as string);
-  if (searchParams.title) sp.set('title', searchParams.title as string);
+  if (searchParams.title) {
+    let title = searchParams.title as string;
+    while (title.endsWith('\n')) title = title.slice(0, -1);
+    sp.set('title', customEncodeURL(title));
+  }
+  if (searchParams.stm) sp.set('stm', searchParams.stm as string);
+  if (searchParams.tps) sp.set('tps', searchParams.tps as string);
+  if (searchParams.icons) sp.set('icons', searchParams.icons as string);
 
   const ogUrl = `/api/og?${sp.toString()}`;
 
