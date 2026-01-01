e9d566a18e1525db3bfdceda85e2527106a6a3ea
Ao1K <averageofonethousand@gmail.com>
2026-01-01 13:52:22 -0600

    Fix path 3
    I don't like these parsing rules

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,7 +3,7 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'daily-scram',
   access: (allow) => ({
-    "/*": [
+    "daily-scram/*": [
       allow.authenticated.to(["read"]),
       allow.guest.to(["read"]),
     ],
