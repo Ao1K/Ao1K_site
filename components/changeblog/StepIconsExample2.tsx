f9775a879a9d958a7676c88c09dd8cd96223953c
Ao1K <averageofonethousand@gmail.com>
2026-03-14 01:05:41 -0500

    Write changeblog 0.7

---
+++ components/changeblog/StepIconsExample2.tsx
@@ -0,0 +1,240 @@
+export function StepIconsExample2_1() {
+  return (
+    <div className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right">
+      <svg viewBox="0 0 24 24"
+        className="step-icon-svg w-full border-2 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none"
+        style={{borderColor: "rgb(255, 0, 255)"}}>
+          <polygon points="0,0 24,0 24,24" fill="#0CEC00"></polygon>
+          <polygon points="0,0 24,24 0,24" fill="#FF7F00"></polygon>
+          <rect x="0" y="0" width="8" height="8" fill="#161018"></rect>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_2() {
+  return (
+    <div id="step-icon-1-L-Block" className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right" style={{height: "36px"}}>
+      <svg viewBox="0 0 24 24"
+        className="step-icon-svg w-full border border-1 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none">
+          <rect x="0" y="0" width="24" height="24" fill="#161018"></rect>
+          <polygon points="10,15 15,15 12,18 7,18" fill="#EEFF00"></polygon>
+          <polygon points="15,15 20,15 17,18 12,18" fill="#888888"></polygon>
+          <polygon points="20,15 25,15 22,18 17,18" fill="#888888"></polygon>
+          <polygon points="7,18 12,18 9,21 4,21" fill="#EEFF00"></polygon>
+          <polygon points="12,18 17,18 14,21 9,21" fill="#888888"></polygon>
+          <polygon points="17,18 22,18 19,21 14,21" fill="#888888"></polygon>
+          <polygon points="4,21 9,21 6,24 1,24" fill="#EEFF00"></polygon>
+          <polygon points="9,21 14,21 11,24 6,24" fill="#888888"></polygon>
+          <polygon points="14,21 19,21 16,24 11,24" fill="#888888"></polygon>
+          <rect x="9" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="14" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="19" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="9" y="5" width="5" height="5" fill="#FF0000"></rect>
+          <rect x="14" y="5" width="5" height="5" fill="#888888"></rect>
+          <rect x="19" y="5" width="5" height="5" fill="#888888"></rect>
+          <rect x="9" y="10" width="5" height="5" fill="#FF0000"></rect>
+          <rect x="14" y="10" width="5" height="5" fill="#888888"></rect>
+          <rect x="19" y="10" width="5" height="5" fill="#888888"></rect>
+          <polygon points="0,9 3,6 3,11 0,14" fill="#888888"></polygon>
+          <polygon points="3,6 6,3 6,8 3,11" fill="#888888"></polygon>
+          <polygon points="6,3 9,0 9,5 6,8" fill="#888888"></polygon>
+          <polygon points="0,14 3,11 3,16 0,19" fill="#0CEC00"></polygon>
+          <polygon points="3,11 6,8 6,13 3,16" fill="#0CEC00"></polygon>
+          <polygon points="6,8 9,5 9,10 6,13" fill="#0CEC00"></polygon>
+          <polygon points="0,19 3,16 3,21 0,24" fill="#0CEC00"></polygon>
+          <polygon points="3,16 6,13 6,18 3,21" fill="#0CEC00"></polygon>
+          <polygon points="6,13 9,10 9,15 6,18" fill="#0CEC00"></polygon>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_3() {
+  return (
+    <div id="step-icon-4-cmll" className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right" style={{height: "36px"}}>
+      <svg viewBox="0 0 21 21"
+        className="step-icon-svg w-full border border-1 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none">
+          <rect x="0" y="0" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="0" width="6" height="3" fill="#003CFF"></rect>
+          <rect x="9" y="0" width="3" height="3" fill="#161018"></rect>
+          <rect x="12" y="0" width="6" height="3" fill="#0CEC00"></rect>
+          <rect x="18" y="0" width="3" height="3" fill="#161018"></rect>
+          <rect x="0" y="3" width="3" height="6" fill="#FFFFFF"></rect>
+          <rect x="3" y="3" width="6" height="6" fill="#FF0000"></rect>
+          <rect x="9" y="3" width="3" height="6" fill="#161018"></rect>
+          <rect x="12" y="3" width="6" height="6" fill="#FF0000"></rect>
+          <rect x="18" y="3" width="3" height="6" fill="#FFFFFF"></rect>
+          <rect x="0" y="9" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="9" width="6" height="3" fill="#161018"></rect>
+          <rect x="9" y="9" width="3" height="3" fill="#161018"></rect>
+          <rect x="12" y="9" width="6" height="3" fill="#161018"></rect>
+          <rect x="18" y="9" width="3" height="3" fill="#161018"></rect>
+          <rect x="0" y="12" width="3" height="6" fill="#FFFFFF"></rect>
+          <rect x="3" y="12" width="6" height="6" fill="#0CEC00"></rect>
+          <rect x="9" y="12" width="3" height="6" fill="#161018"></rect>
+          <rect x="12" y="12" width="6" height="6" fill="#003CFF"></rect>
+          <rect x="18" y="12" width="3" height="6" fill="#FFFFFF"></rect>
+          <rect x="0" y="18" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="18" width="6" height="3" fill="#FF7F00"></rect>
+          <rect x="9" y="18" width="3" height="3" fill="#161018"></rect>
+          <rect x="12" y="18" width="6" height="3" fill="#FF7F00"></rect>
+          <rect x="18" y="18" width="3" height="3" fill="#161018"></rect>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_4() {
+  return (
+    <div id="step-icon-5-solved" className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right" style={{height: "36px"}}>
+      <svg viewBox="0 0 25 24"
+        className="step-icon-svg w-full border border-1 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none">
+          <rect x="0" y="0" width="25" height="24" fill="#161018"></rect>
+          <rect x="0" y="0" width="5" height="4" fill="#FF7F00"></rect>
+          <rect x="5" y="0" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="10" y="0" width="5" height="4" fill="#FF7F00"></rect>
+          <rect x="15" y="0" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="20" y="0" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="0" y="4" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="5" y="4" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="10" y="4" width="5" height="4" fill="#EEFF00"></rect>
+          <rect x="15" y="4" width="5" height="4" fill="#EEFF00"></rect>
+          <rect x="20" y="4" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="0" y="8" width="5" height="4" fill="#FF7F00"></rect>
+          <rect x="5" y="8" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="10" y="8" width="5" height="4" fill="#EEFF00"></rect>
+          <rect x="15" y="8" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="20" y="8" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="5" y="12" width="5" height="4" fill="#0CEC00"></rect>
+          <rect x="10" y="12" width="5" height="4" fill="#FF7F00"></rect>
+          <rect x="15" y="12" width="5" height="4" fill="#0CEC00"></rect>
+          <rect x="0" y="16" width="5" height="4" fill="#0CEC00"></rect>
+          <rect x="5" y="16" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="10" y="16" width="5" height="4" fill="#FF7F00"></rect>
+          <rect x="15" y="16" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="20" y="16" width="5" height="4" fill="#003CFF"></rect>
+          <rect x="0" y="20" width="5" height="4" fill="#0CEC00"></rect>
+          <rect x="5" y="20" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="10" y="20" width="5" height="4" fill="#FFFFFF"></rect>
+          <rect x="15" y="20" width="5" height="4" fill="#FF0000"></rect>
+          <rect x="20" y="20" width="5" height="4" fill="#003CFF"></rect>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_5() {
+  return (
+    <div id="step-icon-6-solved" className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right" style={{height: "36px"}}>
+      <svg viewBox="0 0 24 24"
+        className="step-icon-svg w-full border border-1 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none">
+          <rect x="0" y="0" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="0" width="6" height="3" fill="#003CFF"></rect>
+          <rect x="9" y="0" width="6" height="3" fill="#003CFF"></rect>
+          <rect x="15" y="0" width="6" height="3" fill="#003CFF"></rect>
+          <rect x="21" y="0" width="3" height="3" fill="#161018"></rect>
+          <rect x="0" y="3" width="3" height="6" fill="#FF7F00"></rect>
+          <rect x="3" y="3" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="9" y="3" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="15" y="3" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="21" y="3" width="3" height="6" fill="#FF0000"></rect>
+          <rect x="0" y="9" width="3" height="6" fill="#0CEC00"></rect>
+          <rect x="3" y="9" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="9" y="9" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="15" y="9" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="21" y="9" width="3" height="6" fill="#FF7F00"></rect>
+          <rect x="0" y="15" width="3" height="6" fill="#FF7F00"></rect>
+          <rect x="3" y="15" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="9" y="15" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="15" y="15" width="6" height="6" fill="#FFFFFF"></rect>
+          <rect x="21" y="15" width="3" height="6" fill="#FF0000"></rect>
+          <rect x="0" y="21" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="21" width="6" height="3" fill="#0CEC00"></rect>
+          <rect x="9" y="21" width="6" height="3" fill="#FF0000"></rect>
+          <rect x="15" y="21" width="6" height="3" fill="#0CEC00"></rect>
+          <rect x="21" y="21" width="3" height="3" fill="#161018"></rect>
+          <rect x="3" y="3" width="18" height="18" fill="#FFFFFF"></rect>
+          <text x="12" y="12" textAnchor="middle" dominantBaseline="central" fill="#000000" stroke="none" fontSize="10" fontWeight="bold" fontFamily="var(--font-Rubik), system-ui, sans-serif">Ua</text>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_6() {
+  return (
+    <div id="step-icon-2-R-Square" className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right" style={{height: "36px"}}>
+      <svg viewBox="0 0 24 24"
+        className="step-icon-svg w-full border border-1 border-neutral-600"
+        stroke="#52525b"
+        strokeWidth="1"
+        fill="none">
+          <rect x="0" y="0" width="24" height="24" fill="#161018"></rect>
+          <polygon points="10,15 15,15 12,18 7,18" fill="#FFFFFF"></polygon>
+          <polygon points="15,15 20,15 17,18 12,18" fill="#FFFFFF"></polygon>
+          <polygon points="20,15 25,15 22,18 17,18" fill="#FFFFFF"></polygon>
+          <polygon points="7,18 12,18 9,21 4,21" fill="#FFFFFF"></polygon>
+          <polygon points="12,18 17,18 14,21 9,21" fill="#FFFFFF"></polygon>
+          <polygon points="17,18 22,18 19,21 14,21" fill="#FFFFFF"></polygon>
+          <polygon points="4,21 9,21 6,24 1,24" fill="#888888"></polygon>
+          <polygon points="9,21 14,21 11,24 6,24" fill="#888888"></polygon>
+          <polygon points="14,21 19,21 16,24 11,24" fill="#888888"></polygon>
+          <rect x="9" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="14" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="19" y="0" width="5" height="5" fill="#888888"></rect>
+          <rect x="9" y="5" width="5" height="5" fill="#003CFF"></rect>
+          <rect x="14" y="5" width="5" height="5" fill="#003CFF"></rect>
+          <rect x="19" y="5" width="5" height="5" fill="#003CFF"></rect>
+          <rect x="9" y="10" width="5" height="5" fill="#003CFF"></rect>
+          <rect x="14" y="10" width="5" height="5" fill="#003CFF"></rect>
+          <rect x="19" y="10" width="5" height="5" fill="#003CFF"></rect>
+          <polygon points="0,9 3,6 3,11 0,14" fill="#888888"></polygon>
+          <polygon points="3,6 6,3 6,8 3,11" fill="#888888"></polygon>
+          <polygon points="6,3 9,0 9,5 6,8" fill="#888888"></polygon>
+          <polygon points="0,14 3,11 3,16 0,19" fill="#888888"></polygon>
+          <polygon points="3,11 6,8 6,13 3,16" fill="#FF7F00"></polygon>
+          <polygon points="6,8 9,5 9,10 6,13" fill="#FF7F00"></polygon>
+          <polygon points="0,19 3,16 3,21 0,24" fill="#888888"></polygon>
+          <polygon points="3,16 6,13 6,18 3,21" fill="#FF7F00"></polygon>
+          <polygon points="6,13 9,10 9,15 6,18" fill="#FF7F00"></polygon>
+      </svg>
+    </div>
+  )
+}
+
+export function StepIconsExample2_7() {
+  return (
+    <div 
+    id="step-icon-0-v-eoLine" 
+    className="step-icon-inner w-[80px] h-[80px] flex items-center justify-center overflow-visible transform transition-transform duration-300 ease-out origin-top-right">
+      <svg viewBox="0 0 24 24" className="step-icon-svg w-full border-2" 
+      style={{borderColor: "rgb(255, 0, 255)"}} 
+      stroke="#52525b" strokeWidth="1" fill="none">
+        <rect x="0" y="0" width="8" height="8" fill="#161018"></rect>
+        <rect x="8" y="0" width="8" height="8" fill="#FFFFFF"></rect>
+        <rect x="16" y="0" width="8" height="8" fill="#161018"></rect>
+        <rect x="0" y="8" width="8" height="8" fill="#161018"></rect>
+        <rect x="8" y="8" width="8" height="8" fill="#FFFFFF"></rect>
+        <rect x="16" y="8" width="8" height="8" fill="#161018"></rect>
+        <rect x="0" y="16" width="8" height="8" fill="#161018"></rect>
+        <rect x="8" y="16" width="8" height="8" fill="#FFFFFF"></rect>
+        <rect x="16" y="16" width="8" height="8" fill="#161018"></rect>
+      </svg>
+    </div>
+  )
+}
