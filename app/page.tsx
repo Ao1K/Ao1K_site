83a0b492522c2288a4757d3c960dc13b3ede5884
Ao1K <averageofonethousand@gmail.com>
2026-05-16 18:39:01 -0500

    Fix routing
    routes that do have query params but do not include /recon/ will get fixed for the user automatically.

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,8 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace('/recon');
+    const search = window.location.search;
+    router.replace('/recon' + search);
   }, [router]);
 
   return null; // No need to render anything since we're redirecting
